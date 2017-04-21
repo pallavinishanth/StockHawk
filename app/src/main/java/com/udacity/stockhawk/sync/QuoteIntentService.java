e18ed935a80236c6a618c1331b22e19a9544e6fb
PallaviNishanth <pallavinishanth@gmail.com>
2017-04-21 09:33:26 -0400

    Add Non-Existent Stock requirement

--- app/src/main/java/com/udacity/stockhawk/sync/QuoteIntentService.java
+++ app/src/main/java/com/udacity/stockhawk/sync/QuoteIntentService.java
@@ -2,6 +2,8 @@ package com.udacity.stockhawk.sync;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 import timber.log.Timber;
 
@@ -15,6 +17,11 @@ public class QuoteIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Timber.d("Intent handled");
-        QuoteSyncJob.getQuotes(getApplicationContext());
+        String reply = QuoteSyncJob.getQuotes(getApplicationContext());
+
+        Log.d("sender", "Broadcasting message");
+        Intent mIntent = new Intent("custom-event-name");
+        mIntent.putExtra("message", reply);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(mIntent);
     }
 }
